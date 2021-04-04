8c9f738ecba2a42773163746b9e8498d79bc864f
Xingjian Leng <u7136359@anu.edu.au>
2021-04-04 09:57:58 +0800

    Fix path

--- PastExams/2019-2/Q3/utils/MyUtilTest.java
+++ PastExams/2019-2/Q3/utils/MyUtilTest.java
@@ -12,14 +12,12 @@ import org.junit.Test;
  *
  */
 public class MyUtilTest {
-
 	@Test
 	public void test() {
 		//start your code
 		assertEquals(0, MyUtil.parseDouble(null), 0);
-		assertEquals(0, MyUtil.parseDouble(""), 0);
+		assertEquals(0, MyUtil.parseDouble("aa"), 0);
 		assertEquals(123.456, MyUtil.parseDouble("abc123.456efg"), 0);
-		assertEquals(123, MyUtil.parseDouble("abc123."), 0);
 		//end your code
 	}
 
