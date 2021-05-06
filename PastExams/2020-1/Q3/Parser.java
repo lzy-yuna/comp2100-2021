8e5686e73645402d86a32bd1d21e8249117d8364
Xingjian Leng <u7136359@anu.edu.au>
2021-05-06 08:51:29 +0800

    2020-1/Q3 whole solution

--- PastExams/2020-1/Q3/Parser.java
+++ PastExams/2020-1/Q3/Parser.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * IMPORTANT: This class is incomplete. Please look for "TODO" comments.
  * 
@@ -27,7 +31,41 @@ public class Parser {
 
 		// TODO: Complete this method
 		// START YOUR CODE
-		
+		if (tokeniser.hasNext()) {
+			Token nextCommand = tokeniser.takeNext();
+
+			if (nextCommand != null) {
+				if (nextCommand.type == Token.Type.INSERT_INTO) {
+					String tableName = nextCommand.value.split(" ")[0];
+					String columnString = nextCommand.value.split(" ")[1];
+					boolean validColumns = true;
+					String[] columns = columnString.substring(1, columnString.length() - 1).split(",");
+					List<String> columnsTrue = Arrays.asList(Customer.KEY_ID, Customer.KEY_NAME, Customer.KEY_CITY,
+						Customer.KEY_ADDRESS, Customer.KEY_POSTCODE, Customer.KEY_COUNTRY);
+					for (String column : columns) {
+						if (!columnsTrue.contains(column)) {
+							validColumns = false;
+							break;
+						}
+					}
+					if (validColumns) {
+						if (tokeniser.hasNext()) {
+							Token values = tokeniser.takeNext();
+							if (values.type == Token.Type.VALUES) {
+								String insertValueBracket = values.value;
+								String[] insertValues = insertValueBracket.substring(1, insertValueBracket.length() - 1).split(",");
+								for (int i  = 0; i < insertValues.length; i++) {
+									insertValues[i] = insertValues[i].replace("'", "").strip();
+								}
+								Customer c = new Customer(Integer.parseInt(insertValues[0]), insertValues[1], insertValues[2],
+										insertValues[3], insertValues[4], insertValues[5]);
+								table.insert(tableName, c);
+							}
+						}
+					}
+				}
+			}
+		}
 
 		// END YOUR CODE
 	}
