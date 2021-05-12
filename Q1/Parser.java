62dbd1fe4b64b848e05060893052e628ca88f3ea
Xingjian Leng <u7136359@anu.edu.au>
2021-05-13 00:58:24 +0800

    Conceptual problems

--- Q1/Parser.java
+++ Q1/Parser.java
@@ -26,10 +26,40 @@ public class Parser {
         // TODO: Complete this method
 		// START YOUR CODE
 
+		// For the above Grammar, it is ambiguous
+		// We can convert it into: S := ()A | )S(A | () | )S( ; A := SA
+		// which is equivalent to S := ()A | )S(A ; A := SA | ε
+		Token.Type nextType = _tokeniser.takeNext().type;
 
+		if (nextType == Token.Type.LEFT_BRACKET) {
+			if (_tokeniser.hasNext()) {
+				return _tokeniser.takeNext().type == Token.Type.RIGHT_BRACKET && parseA();
+			} else {
+				return false;
+			}
+		}
+
+		if (nextType == Token.Type.RIGHT_BRACKET) {
+			if (_tokeniser.hasNext()) {
+				return parseExp() && _tokeniser.takeNext().type == Token.Type.LEFT_BRACKET && parseA();
+			} else {
+				return false;
+			}
+		}
 
 		// END YOUR CODE
 		return false;
     }
+
+	/**
+	 * Parser for A
+	 * @return whether the text in tokenizer is valid
+	 */
+	private boolean parseA() {
+		if (!_tokeniser.isNextValid()) return false;
+		if (!_tokeniser.hasNext()) return true;
+
+		return parseExp() && parseA();
+	}
 
 }
